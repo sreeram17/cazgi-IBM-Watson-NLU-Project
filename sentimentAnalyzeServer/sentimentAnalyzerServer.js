d10c928e8f7b63b477abf423e6bdd4769a0a3627
Sree Ram Mohanan <sreeram17@gmail.com>
2021-01-29 18:32:18 +0000

    update NLU instance instantiation

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
 const app = new express();
+
+function getNLUInstance(){
+    let api_key = process.env.api_key;
+    let api_url = process.env.api_url;
+
+    const NaturalLanguageUnderstandV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require ('ibm-watson/auth');
+    
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandV1({
+        version: '2021-01-29',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    })
+    return naturalLanguageUnderstanding;
+}
 
 app.use(express.static('client'))
 
